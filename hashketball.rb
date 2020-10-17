e120bf69ffd7e549783111428a2df2cd28f39e50
Marcia Freeman <marciafreeman2011@gmail.com>
2020-10-17 15:18:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -183,18 +183,16 @@ def player_numbers(team_name)
   
   game_hash.each do |key_home, value|
     value.find do
+      
       if value[:team_name] == team_name
-  
-        test1 = value[:players][0]
         player = value[:players]
-        
         player.each do |element|
+          test1 = element[:number]
+          jersey_n_array << value[:team_name][]
           binding.pry
         end
-        
-        #jersey_n_array << value[:team_name][]
-        binding.pry
       end
+      
     end
   end
     jersey_n_array
